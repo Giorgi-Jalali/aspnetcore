6d1ff77aae053d69acf487a145c3e05a171d5edc
giorgi <jalala18@mail.ru>
2024-10-23 03:05:58 +0400

    add simple calculator

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -2,11 +2,51 @@
 
 namespace SimpleCalculator
 {
-  internal class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Hello World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("მოგესალმებით მარტივ კალკულატორში!");
+
+            Console.Write("შეიყვანეთ პირველი რიცხვი: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("შეიყვანეთ მეორე რიცხვი: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("აირჩიეთ ოპერაცია:");
+            Console.WriteLine("1 - მიმატება");
+            Console.WriteLine("2 - გამოკლება");
+            Console.WriteLine("3 - გამრავლება");
+            Console.WriteLine("4 - გაყოფა");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0;
+
+            switch (choice)
+            {
+                case 1:
+                    result = number1 + number2;
+                    break;
+                case 2:
+                    result = number1 - number2;
+                    break;
+                case 3:
+                    result = number1 * number2;
+                    break;
+                case 4:
+                    if (number2 != 0)
+                        result = number1 / number2;
+                    else
+                        Console.WriteLine("გაყოფა ნულზე შეუძლებელია!");
+                    break;
+                default:
+                    Console.WriteLine("არასწორი არჩევანი!");
+                    return;
+            }
+
+            Console.WriteLine("შედეგი: " + result);
+        }
     }
-  }
 }
--- SimpleCalculator/SimpleCalculator/obj/Debug/net5.0/SimpleCalculator.AssemblyInfo.cs
+++ SimpleCalculator/SimpleCalculator/obj/Debug/net5.0/SimpleCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69f96502f6ee96491541927baf20ab225a7a0ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3812cf9c5c526af17779551271a0ff2a5f680896")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
